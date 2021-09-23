a3099acfc7960b030a62d66d53007b8445de628d
Eric Chan <ckc261@ha.org.hk>
2021-09-23 15:31:16 +0800

    finish pick up plastic card

--- src/components/cards/plasticcard.component.tsx
+++ src/components/cards/plasticcard.component.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, CSSProperties, useState, useLayoutEffect, useCallback } from 'react';
+import { ReactNode, CSSProperties, MouseEvent, useState, useLayoutEffect, useCallback } from 'react';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 export interface PlasticCardStyles {
@@ -19,16 +19,26 @@ export default function PlasticCard(props: PlasticCardProps) {
       borderRadius: '16px',
       border: '1px solid white',
       transform,
-      transition: 'transform 1s',
+      transition: 'all 1s cubic-bezier(.25,.8,.25,1)',
     },
   };
 
   const defaultCardOrientation = useCallback(() => {
     setTransform('rotate(0deg)');
   }, []);
-  
+
   const changeCardOrientation = useCallback(() => {
     setTransform('rotate(90deg)');
+  }, []);
+
+  const handleMouseEnter = useCallback((event: MouseEvent<HTMLDivElement>) => {
+    event.currentTarget.style.transform = 'translateY(-16px)';
+    event.currentTarget.style.boxShadow = '0 19px 38px rgba(255,255,255,0.30), 0 15px 12px rgba(255,255,255,0.22)';
+  }, []);
+
+  const handleMouseLeave = useCallback((event: MouseEvent<HTMLDivElement>) => {
+    event.currentTarget.style.transform = 'translateY(0)';
+    event.currentTarget.style.boxShadow = '';
   }, []);
 
   useLayoutEffect(() => {
@@ -38,9 +48,9 @@ export default function PlasticCard(props: PlasticCardProps) {
       defaultCardOrientation();
     }
   })
-  
+
   return (
-    <div className='plastic-card' style={styles.card}>
+    <div className='plastic-card' style={styles.card} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {props.children}
     </div>
   )
