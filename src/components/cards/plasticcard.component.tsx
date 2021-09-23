27648f1f3c44b255a621475066fc2067c60c22de
Eric Chan <ckc261@ha.org.hk>
2021-09-23 15:05:10 +0800

    add windows dimensions hook. add card rotation

--- src/components/cards/plasticcard.component.tsx
+++ src/components/cards/plasticcard.component.tsx
@@ -1,14 +1,46 @@
-import React, { ReactNode } from 'react';
+import { ReactNode, CSSProperties, useState, useLayoutEffect, useCallback } from 'react';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
-import './plasticcard.scss';
-
+export interface PlasticCardStyles {
+  card: CSSProperties;
+}
 export interface PlasticCardProps {
   children?: ReactNode;
 }
 
 export default function PlasticCard(props: PlasticCardProps) {
+  const { innerWidth, innerHeight } = useWindowDimensions();
+  const [transform, setTransform] = useState<string>('');
+
+  const styles: PlasticCardStyles = {
+    card: {
+      width: '90mm',
+      height: '54mm',
+      borderRadius: '16px',
+      border: '1px solid white',
+      transform,
+      transition: 'transform 1s',
+    },
+  };
+
+  const defaultCardOrientation = useCallback(() => {
+    setTransform('rotate(0deg)');
+  }, []);
+  
+  const changeCardOrientation = useCallback(() => {
+    setTransform('rotate(90deg)');
+  }, []);
+
+  useLayoutEffect(() => {
+    if (innerHeight > innerWidth) {
+      changeCardOrientation();
+    } else {
+      defaultCardOrientation();
+    }
+  })
+  
   return (
-    <div className="plastic-card">
+    <div className='plastic-card' style={styles.card}>
       {props.children}
     </div>
   )
